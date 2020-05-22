9355dbf81eed626dbdb704dc6924e21676023156
Alice <ciceribeiroo@gmail.com>
2020-05-22 09:07:29 -0300

    Adicionando projeto
    erro ao ir pra ViewAll depois do Edit

---
+++ jQueryAjaxAsp.NETMVC/jQueryAjaxAsp.NETMVC/Controllers/EmployeeController.cs
@@ -0,0 +1,103 @@
+﻿using jQueryAjaxAsp.NETMVC;
+using jQueryAjaxAsp.NETMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace jQueryAjaxInAsp.NETMVC.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        //
+        // GET: /Employee/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ViewAll()
+        {
+            return View(GetAllEmployee());
+        }
+
+        IEnumerable<Employee> GetAllEmployee()
+        {
+            using (DBModel db = new DBModel())
+            {
+                return db.Employee.ToList<Employee>();
+            }
+
+        }
+
+        public ActionResult AddOrEdit(int id = 0)
+        {
+            Employee emp = new Employee();
+            if (id != 0)
+            {
+                using (DBModel db = new DBModel())
+                {
+                    emp = db.Employee.Where(x => x.EmployedID == id).FirstOrDefault<Employee>();
+                }
+            }
+            return View(emp);
+        }
+
+        [HttpPost]
+        public ActionResult AddOrEdit(Employee emp)
+        {
+            try
+            {
+                if (emp.imageUpload != null)
+                {
+                    string fileName = Path.GetFileNameWithoutExtension(emp.imageUpload.FileName);
+                    string extension = Path.GetExtension(emp.imageUpload.FileName);
+                    fileName = fileName + DateTime.Now.ToString("yymmssfff") + extension;
+                    emp.ImagePath = "~/AppFiles/Images/" + fileName;
+                    emp.imageUpload.SaveAs(Path.Combine(Server.MapPath("~/AppFiles/Images/"), fileName));
+                }
+                using (DBModel db = new DBModel())
+                {
+                    if (emp.EmployedID == 0)
+                    {
+                        db.Employee.Add(emp);
+                        db.SaveChanges();
+                    }
+                    else
+                    {
+                        db.Entry(emp).State = EntityState.Modified;
+                        db.SaveChanges();
+
+                    }
+                }
+                return Json(new { success = true, html = GlobalClass.RenderRazorViewToString(this, "ViewAll", GetAllEmployee()), message = "Submitted Successfully" }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+
+                return Json(new { success = false, message = ex.Message }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                using (DBModel db = new DBModel())
+                {
+                    Employee emp = db.Employee.Where(x => x.EmployedID == id).FirstOrDefault<Employee>();
+                    db.Employee.Remove(emp);
+                    db.SaveChanges();
+                }
+                return Json(new { success = true, html = GlobalClass.RenderRazorViewToString(this, "ViewAll", GetAllEmployee()), message = "Deleted Successfully" }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                return Json(new { success = false, message = ex.Message }, JsonRequestBehavior.AllowGet);
+            }
+        }
+    }
+}
---
+++ jQueryAjaxAsp.NETMVC/jQueryAjaxAsp.NETMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="jQueryAjaxAsp.NETMVC.MvcApplication" Language="C#" %>
